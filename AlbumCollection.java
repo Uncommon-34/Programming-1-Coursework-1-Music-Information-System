69b3d4c5a453b87947e64a812f57bce1f8b4a46b
Uncommon-34 <84247691+Uncommon-34@users.noreply.github.com>
2024-11-22 10:57:33 +0000

    added album collections

--- AlbumCollection.java
+++ AlbumCollection.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlbumCollection {
   //store details of a colluctoin of almbums
   //member variable of Albums
   //methods for awnsering general queries
+
+
+ private final String artist;
+  private final List<Album> albums = new ArrayList<>();
+
+  public AlbumCollection(String artist) {
+    this.artist = artist;
+  }
+
+  public void addAlbum(Album album) {
+    albums.add(album);
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public List<Album> getAlbums() {
+    return albums;
+  }
+
+  public String getAlbumCollectionNameString() {
+    return artist;
+  }
+
 }
+
