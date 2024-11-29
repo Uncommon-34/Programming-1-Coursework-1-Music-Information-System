87f0a32433e959933f3561cc0a9d67fdd8b1c3e0
Uncommon-34 <84247691+Uncommon-34@users.noreply.github.com>
2024-11-29 11:07:59 +0000

    added test harnes to album collection

--- AlbumCollection.java
+++ AlbumCollection.java
@@ -26,7 +26,37 @@ public class AlbumCollection {
     return albums;
   }
 
-  public String getAlbumCollectionNameString() {
-    return artist;
+  //test harness
+  public static void main(String[] args) {
+    Album albumObjectOne = new Album(
+      "Kraftwerk",
+      "COMPUTER WORLD",
+      1981
+    );
+    Album albumObjectTwo = new Album(
+      "Kraftwerk",
+      "DIE MENSCH-MASCHINE",
+      1978
+    );
+
+    //no need to add tracks as no track methords to test
+
+    AlbumCollection albumCollectionObject = new AlbumCollection("Kraftwerk"); // artist name is the title of the collection
+    albumCollectionObject.addAlbum(albumObjectOne);
+    albumCollectionObject.addAlbum(albumObjectTwo);
+
+    System.out.println(
+      "This is a test harness function for the AlbumCollection class\nExample of getArtist:\n" +
+      albumCollectionObject.getArtist() +
+      "\n\nExample of getAlbums:\n" +
+      albumCollectionObject.getAlbums() +
+      "\n\nputting them albums to strings:"
+    );
+
+    List<Album> Albums = albumCollectionObject.getAlbums();
+        for (Album album : Albums) {
+          System.out.println(album.stringAlbumInfo());
+        }
   }
 }
+
